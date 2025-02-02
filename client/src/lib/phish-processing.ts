b6e6a13a702f1c4a23f79f73063cf5fa57676a2c
mattcasella <39297843-mattcasella@users.noreply.replit.com>
2025-02-02 23:31:02 +0000

    User checkpoint: Add function to extract unique songs from setlists, handling potential duplicate songs within a show.

---
+++ client/src/lib/phish-processing.ts
@@ -0,0 +1,40 @@
+interface Song{
+  string: name,
+  date: showdate
+}
+
+
+export function getUniqueSongsFromSetlists(setlists: any) {
+  let allMySongs = new Map()
+
+  setlists.forEach(setlist => {
+      let songsFromShow = new Map()
+
+      setlist.forEach(song => {
+          //console.log('processing %s', name)
+
+          let name = song.song
+          let songEntry = {name: song.song, date: song.showdate}
+
+          //first time
+          if(allMySongs.has(name) === false){
+              allMySongs.set(name, [songEntry])
+              songsFromShow.set(name, [songEntry])
+          }
+          else{
+              let songEntries = allMySongs.get(name)
+
+              //TODO: handle multiple songs at same show
+              if(songsFromShow.has(name)){
+                  console.log('repeat song %s on %s', songEntry.name, songEntry.date)
+              }
+
+              songEntries.push([songEntry])
+          }
+      })
+
+  })
+  allMySongs = new Map([...allMySongs.entries()].sort());
+
+  return allMySongs
+}
